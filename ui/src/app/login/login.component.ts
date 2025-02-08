1041f1044303dea1a03a2a6d20bd634f2d61321d
KipCollo <kipkosgei.tech@gmail.com>
2025-02-08 12:44:48 +0300

    feat: finalize authentication

--- ui/src/app/login/login.component.ts
+++ ui/src/app/login/login.component.ts
@@ -6,6 +6,7 @@ import {Router} from "@angular/router";
 import {AuthenticationService} from "../services/services/authentication.service";
 import {AuthenticationResponse} from "../services/models/authentication-response";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {TokenService} from "../services/token/token.service";
 
 @Component({
   selector: 'app-login',
@@ -25,6 +26,7 @@ export class LoginComponent {
 
   constructor(private router: Router,
               private authService: AuthenticationService,
+              private tokenService: TokenService
               )
               {
   }
@@ -35,7 +37,7 @@ export class LoginComponent {
       body: this.authRequest
     }).subscribe({
       next: (res) =>{
-        //this.tokenService.token = res.token as string;
+        this.tokenService.token = res.token as string;
         this.router.navigate(['home']);
       },
       error: (err) => {
@@ -43,7 +45,7 @@ export class LoginComponent {
         if(err.error.validationErrors){
           this.errorMsg = err.error.validationErrors;
         } else {
-          this.errorMsg.push(err.error.errorMsg);
+          this.errorMsg.push(err.error.error);
         }
       }
     });
--- ui/src/app/register/register.component.ts
+++ ui/src/app/register/register.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import {CustomerRequest} from "../services/models/customer-request";
+import {FormsModule} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../services/services/authentication.service";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  customer: CustomerRequest ={firstName: '',lastName: '',email: '',password: ''}
+  errorMsg: Array<string> =[]
+
+  constructor(private router: Router,
+              private authService: AuthenticationService) {
+  }
+
+  register(){
+    this.errorMsg = []
+    this.authService.register({
+      body: this.customer
+    }).subscribe({
+      next: (res) =>{
+        this.router.navigate(["activate-account"]);
+      },
+      error: (err) => {
+        this.errorMsg = err.error.validationErrors;
+      }
+    })
+
+  }
+  login() {
+    this.router.navigate(['login']);
+  }
 }
