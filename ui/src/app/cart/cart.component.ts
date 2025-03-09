871153d0bede2c6ec790e929cd40253921001595
KipCollo <kipkosgei.tech@gmail.com>
2025-03-10 01:43:33 +0300

    update security

--- ui/src/app/cart/cart.component.ts
+++ ui/src/app/cart/cart.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {CurrencyPipe, NgForOf, NgIf} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrencyPipe,
+    RouterLink,
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+
+  isLoggedIn = true; // Change this based on authentication status
+  cartItems = [
+    {id:1 ,name: 'Product 1', price: 19.99 },
+    {id:2, name: 'Product 2', price: 29.99 }
+  ];
+
+  // constructor() {
+  //   // Simulating authentication check
+  //   this.isLoggedIn = !!localStorage.getItem('userToken');
+  // }
+  //
+  // cartItems = this.cartService.getCartItems();
+  //
+  // constructor(private cartService: CartService) {}
+  //
+  removeFromCart(productId: number) {
+   // this.cartService.removeFromCart(productId);
+  }
+
+  placeOrder() {
+    alert('Order placed successfully!');
+  }
 
 }
--- ui/src/app/user/user.component.ts
+++ ui/src/app/user/user.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
+import {DatePipe, NgForOf, NgIf} from "@angular/common";
+import Swiper from "swiper";
+import {Navigation} from "swiper/modules";
+
+
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf,
+    DatePipe,
+    RouterLink
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent{
+  user: any = {name:"Collins Kipkosgei"};
+  orders = [
+    { name: 'Order 1', status: 'Pending' },
+    { name: 'Order 2', status: 'Shipped' },
+    { name: 'Order 3', status: 'Delivered' },
+  ];
+
+  constructor( private router: Router) {}
+
+  ngOnInit() {
+    // this.authService.getProfile().subscribe(data => {
+    //   this.user = data;
+    // });
+  }
+
+  logout() {
+    // this.authService.logout();
+    // this.router.navigate(['/login']);
+  }
 
 }
