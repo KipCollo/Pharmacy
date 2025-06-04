dd77c55fb531677651eeaf70057bb36ae3d61135
KipCollo <kipkosgei.tech@gmail.com>
2025-06-04 13:45:46 +0300

    update DevOps tools

---
+++ api/src/test/java/com/kipcollo/cart/CartControllerTest.java
@@ -0,0 +1,38 @@
+package com.kipcollo.cart;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getUserCart() {
+    }
+
+    @Test
+    void getCartsByStatus() {
+    }
+
+    @Test
+    void addCart() {
+    }
+
+    @Test
+    void removeFromCart() {
+    }
+
+    @Test
+    void placeOrder() {
+    }
+}
---
+++ ui/react-ui/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Service from "./Service.tsx";
+
+function App() {
+
+  return (
+    <>
+        <Service/>
+    </>
+  )
+}
+
+export default App
